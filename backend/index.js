29c87b70a868d907a30a583bb7f783a59fc99f38
Aadidev Soni <aadidevbahrain@gmail.com>
2025-07-05 15:51:01 +0300

    idk

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: https://gro-academy-backend.onrender.com, 
+  origin: 'http://localhost:5174', 
   credentials: true,                
 }));
 
